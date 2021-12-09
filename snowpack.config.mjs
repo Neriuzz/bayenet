901efecd4c212bd11bd2cd81418d6bfc4c3632e0
Neriuzz <neriusilmonas@gmail.com>
2021-12-09 23:13:03 +0100

    More configuration stuff

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,6 +1,12 @@
 export default {
-    mount: {
-        public: { url: "/", static: true },
-        src: "/dist",
-    },
-}
+	mount: {
+		public: { url: "/", static: true },
+		src: "/dist",
+	},
+	alias: {
+		"@": "./src"
+	},
+	plugins: [
+		"@snowpack/plugin-react-refresh"
+	]
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
 
-const App = () => {
-    return (
-        <>
-            <h1>Test</h1>
-        </>
-    );
-};
+import App from "@/components/App";
 
 render(<App/>, document.getElementById("root"));
-
+   
 // Hot Module Replacement
 if (undefined /* [snowpack] import.meta.hot */ ) {
-    // @ts-ignore
-    undefined /* [snowpack] import.meta.hot */ .accept();
+	// @ts-ignore
+	undefined /* [snowpack] import.meta.hot */ .accept();
 }
