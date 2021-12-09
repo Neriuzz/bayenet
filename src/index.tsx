d95afa04e3e28512646ad46cbe7c96ea61128ec1
Neriuzz <neriusilmonas@gmail.com>
2021-12-09 22:44:46 +0100

    Enable HMR & perform CSS reset

--- src/index.tsx
+++ src/index.tsx
@@ -10,3 +10,9 @@ const App = () => {
 };
 
 render(<App/>, document.getElementById("root"));
+
+// Hot Module Replacement
+if (undefined /* [snowpack] import.meta.hot */ ) {
+    // @ts-ignore
+    undefined /* [snowpack] import.meta.hot */ .accept();
+}
