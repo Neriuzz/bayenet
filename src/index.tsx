43d00b10bf91a47104bcf68ba99bb7d161dcdaf5
Neriuzz <neriusilmonas@gmail.com>
2021-12-09 22:27:04 +0100

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => {
+    return (
+        <>
+            <h1>Test</h1>
+        </>
+    );
+};
+
+render(<App/>, document.getElementById("root"));
