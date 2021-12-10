7d9ae19f57145f21c44d779c8e6be25e7d41c53a
Neriuzz <neriusilmonas@gmail.com>
2021-12-10 03:07:36 +0100

    Clean up

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,24 +2,12 @@
 import "@styles/App.scss";
 
 // React
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const App: React.FC = () => {
-
-	const [count, setCount] = useState(0);
-	useEffect(() => {
-		const timer = setTimeout(() => setCount(count + 1), 1000);
-		return () => clearTimeout(timer);
-	}, [count, setCount]);
-
 	return (
 		<>
-			<h1 className="big">
-				test
-			</h1>
-			<p>
-				Page has been opened for {count} seconds.
-			</p>
+			<canvas id="canvas"></canvas>
 		</>
 	);
 };
