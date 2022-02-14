45b3d6ffe7434830af1a6efa8db634121e65d8b4
Neriuzz <neriusilmonas@gmail.com>
2022-02-14 19:28:50 +0000

    rudimentary camera system

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,12 +1,23 @@
+import Renderer from "@/renderer/Renderer";
+import "@styles/Canvas.scss";
 import { useEffect, useRef } from "react";
 
 const Canvas = () => {
 
 	const canvasRef = useRef<HTMLCanvasElement | null>(null);
+	const contextRef = useRef<CanvasRenderingContext2D | null | undefined>(null);
+	const rendererRef = useRef<Renderer | null>(null);
 
 	useEffect(
 		() => {
-			const canvas = canvasRef.current;
+			contextRef.current = canvasRef.current?.getContext("2d");
+
+			if (canvasRef.current && contextRef.current) {
+				// TODO: Register canvas and context with renderer, register event listeners
+				rendererRef.current = new Renderer(canvasRef.current, contextRef.current);
+			} else {
+				console.log("Something went terribly wrong");
+			}
 		},
 		[]
 	);
--- src/renderer/Renderer.ts
+++ src/renderer/Renderer.ts
@@ -1,2 +1,36 @@
+import Camera from "./Camera";
+
 export default class Renderer {
+
+	canvas: HTMLCanvasElement;
+	context: CanvasRenderingContext2D;
+	camera: Camera;
+
+	constructor(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
+		this.canvas = canvas;
+		this.context = context;
+		this.resize();
+		this.drawCircle();
+
+		this.camera = new Camera(this.canvas, this.context);
+
+		window.addEventListener("resize", () => this.resize());
+	}
+
+	drawCircle() {
+		console.log("xd");
+		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		this.context.beginPath();
+		this.context.fillStyle = "red";
+ 		this.context.arc(this.canvas.width / 2 - (50 / 2), this.canvas.height / 2 - (50 / 2), 50, 0, Math.PI * 2);
+		this.context.fill();
+		window.requestAnimationFrame(() => this.drawCircle());
+	}
+
+	resize() {
+		this.canvas.width = window.innerWidth * window.devicePixelRatio;
+		this.canvas.height = window.innerHeight * window.devicePixelRatio;
+		this.canvas.style.transform = `scale(${1 / window.devicePixelRatio})`;
+	}
+
 }
