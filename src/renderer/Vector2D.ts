599bbfe1c7a499944c74d15241901e8e63879668
Neriuzz <neriusilmonas@gmail.com>
2022-03-11 18:42:21 +0000

    Change Vector2D

--- src/renderer/Vector2D.ts
+++ src/renderer/Vector2D.ts
@@ -1,14 +1,3 @@
 export default class Vector2D {
-	x: number;
-	y: number;
-
-	constructor(x: number, y: number) {
-		this.x = x;
-		this.y = y;
-	}
-
-	public offset(offset: Vector2D) {
-		this.x -= offset.x;
-		this.y -= offset.y;
-	}
+	constructor(public x: number, public y: number) {}
 }
