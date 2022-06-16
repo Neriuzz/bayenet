4ac8a4cd6db7266d80c36a59f05c3e2b2d2d37ea
Neriuzz <neriusilmonas@gmail.com>
2022-06-16 22:00:05 +0100

    Add node states

--- src/components/NodeStates/NodeStates.tsx
+++ src/components/NodeStates/NodeStates.tsx
@@ -1,12 +1,37 @@
 import { StateProbabilities } from "../../shared/DataModels/bayesian-node";
 
+// For probability bar colours
+import ColorHash from "color-hash";
+
+// Styles
+import "./NodeStates.scss";
 export interface NodeStatesProps {
     states: string[];
     stateProbabilities: StateProbabilities;
 }
 
 const NodeStates = ({ states, stateProbabilities }: NodeStatesProps) => {
-    return null;
+    const colourHash = new ColorHash();
+
+    return (
+        <div className="node-states">
+            {states.map((state, index) => (
+                <div key={index} className="node-state">
+                    <p className="state-name" title={state}>
+                        {state}
+                    </p>
+                    <div
+                        className="state-probability-bar"
+                        style={{
+                            width: `${Math.floor(stateProbabilities[state] * 100)}%`,
+                            backgroundColor: colourHash.hex(state)
+                        }}
+                    ></div>
+                    <p className="state-probability">{`${Math.floor(stateProbabilities[state] * 100)}%`}</p>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default NodeStates;
